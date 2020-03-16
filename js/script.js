86bbd661256d12b0ce50efd3cbba70d8a130001a
superwierdo <superwierdo@hotmail.com>
2020-03-16 15:15:28 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  let naam = "naam";
+  let achternaam = "achternaam";
+  let rij = 1;
+  let formRij =
+    '<div class="d-flex flex-row rij">' +
+    '<label for="temp">voornaam</label>' +
+    '<input type="text" id="temp" />' +
+    '<label for="temp1">Achternaam</label>' +
+    '<input type="text" id="temp1" />' +
+    '<button class="delete btn">X</button>' +
+    "</div>";
+
+  $("form").on("submit", false);
+  $(".ok").on("click", add);
+  $(document.body).on("click", ".delete", remove);
+
+  function add() {
+    //maak nieuwe inputs
+    $("form").append(formRij);
+    $('div[class="d-flex flex-row rij"]').attr("class", `rij${rij}`);
+    $('label[for="temp"]').attr("for", naam + rij);
+    $('input[id="temp"]').attr("id", naam + rij);
+    $('label[for="temp1"]').attr("for", achternaam + rij);
+    $('input[id="temp1"]').attr("id", achternaam + rij);
+    $("button[]");
+
+    rij++;
+  }
+
+  function remove() {
+    $(".rij" + (rij - 1)).remove();
+    rij--;
+
+    console.log("remove einde");
+  }
+});
