37c5cd42b8b4521124da3c9f088f4aa100be7e94
superwierdo <superwierdo@hotmail.com>
2020-03-16 15:56:08 +0100

    added bootstrap form classes, added team check

--- js/script.js
+++ js/script.js
@@ -4,32 +4,36 @@ $(document).ready(function() {
   let rij = 1;
   let formRij =
     '<div class="d-flex flex-row rij">' +
-    '<label for="temp">voornaam</label>' +
-    '<input type="text" id="temp" />' +
-    '<label for="temp1">Achternaam</label>' +
-    '<input type="text" id="temp1" />' +
+    '<div class="form-group">' +
+    '<label class="mr-1" for="temp">Voornaam</label>' +
+    '<input class="mr-1" type="text" id="temp" />' +
+    '<label class="mr-1" for="temp1">Achternaam</label>' +
+    '<input class="mr-1" type="text" id="temp1" />' +
     '<button class="delete btn">X</button>' +
+    "</div>" +
     "</div>";
 
   $("form").on("submit", false);
   $(".ok").on("click", add);
   $(document.body).on("click", ".delete", remove);
 
+  // voeg een nieuwe rij teamvelden toe
   function add() {
-    //maak nieuwe inputs
-    $("form").append(formRij);
-    $('div[class="d-flex flex-row rij"]').attr("class", `rij${rij}`);
-    $('label[for="temp"]').attr("for", naam + rij);
-    $('input[id="temp"]').attr("id", naam + rij);
-    $('label[for="temp1"]').attr("for", achternaam + rij);
-    $('input[id="temp1"]').attr("id", achternaam + rij);
-    rij++;
+    if (rij === 15) {
+      alert("Er mogen maximaal 15 teamleden worden toegevoegd");
+    } else {
+      $("form").append(formRij);
+      $('div[class="d-flex flex-row rij"]').attr("class", `rij${rij}`);
+      $('label[for="temp"]').attr("for", naam + rij);
+      $('input[id="temp"]').attr("id", naam + rij);
+      $('label[for="temp1"]').attr("for", achternaam + rij);
+      $('input[id="temp1"]').attr("id", achternaam + rij);
+      rij++;
+    }
   }
-
+  // verwijder onderste rij van teamvelden.
   function remove() {
     $(".rij" + (rij - 1)).remove();
     rij--;
-
-    console.log("remove einde");
   }
 });
